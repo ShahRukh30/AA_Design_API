121041fcc9a028a50d5ae8de048af92d62a7d81f
mham7 <m.hammadcr7777@gmail.com>
2024-04-17 19:25:16 +0500

    RPC done

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -2,6 +2,8 @@
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Models.SupabaseModels;
+using Newtonsoft.Json;
+using Supabase.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,8 @@ namespace Infrastructure.Repositories
     //    .Include(p => p.Productimages)
     //    .Include(p => p.Productcategory)
     //    .FirstOrDefaultAsync(p=>p.Productid==id);
+
+
     public class ProductRepository : IProductRepository
     {
         
@@ -25,33 +29,22 @@ namespace Infrastructure.Repositories
         }
         public async Task<object> GetDetailsById(int id)
         {
-            var query= from product in _context.Products
-                       join category in _context.Productcategories on product.Productcategoryid equals category.Productcategoryid
-                       //join productsize in _context.Productsizes on product.Productid equals productsize.Productid
-                       join image in _context.Productimages on product.Productid equals image.Productid
-                       where image.Ismainimage == true
-                       select new
-                       {
-                           ProductId = product.Productid,
-                           ProductName = product.Productname,
-                           ProductDescription=product.Productdescription,
-                           ProductQuantity=product.Quantity,
-                           CategoryID=product.Productcategoryid,
-                           ProductPrice = product.Price,
-                           CategoryName = category.Productcategory1,
-                           ImageUrl = image.Imageurl,
-                           ProductSizes = _context.Productsizes
-                        .Where(ps => ps.Productid == product.Productid)
-                         .Select(ps => new 
-                         {
-                             SizeId = ps.Sizeid,
-                             SizeQuantity = ps.Sizequantity
-                         })
-                                    .ToList()
-                       };
-            return await query.FirstOrDefaultAsync();
-          
+            var url = "https://lbqpoifccgmqlsydhvke.supabase.co";
+            var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxicXBvaWZjY2dtcWxzeWRodmtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2MDE4NDYsImV4cCI6MjAyODE3Nzg0Nn0.EkMkG3X1inJUPs0Y0_GFCnVBCidQ2VMtTlINCw3Xu8A";
+            var options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+
+            var supabase = new Supabase.Client(url, key, options);
+            await supabase.InitializeAsync();
+
+            var functionName = "get_product_details"; // Adjusted function name
+            var response = await supabase.Rpc(functionName, new Dictionary<string, object> { {"input_id", id} }); // Adjusted parameter name
+            var details = JsonConvert.DeserializeObject<object>(response.Content);
+            return details;
         }
+    
 
         public async Task<List<object>> GetProductListing()
         {
