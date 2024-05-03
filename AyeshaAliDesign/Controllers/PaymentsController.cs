9ef7e43dcce9d27788e84cbb9a31d61b9aafdecc
mham7 <m.hammadcr7777@gmail.com>
2024-05-03 16:40:35 +0500

    webhook testing

--- AyeshaAliDesign/Controllers/PaymentsController.cs
+++ AyeshaAliDesign/Controllers/PaymentsController.cs
@@ -27,17 +27,37 @@ namespace API.Controllers
             return _stripe.CreateCheckoutSession(amount,email,orderid);
         }
 
+        [HttpPost("Check")]
+        public async Task<IActionResult> Check()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            var endpointSecret = "whsec_stA2aWoTuvWcJv9zII2MSgCTvRmvniUh";
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                    Request.Headers["Stripe-Signature"], endpointSecret);
 
-      
+                // Handle the event
+                Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+        }
+
+
         [HttpPost("Payment-Event")]
         public async Task<IActionResult> Webhook()
         {
-            var endpointSecret = "whsec_92a50f4783a166c8fa914b3eec19c89bfd06c02c39ae32fc5529a460b7c4adfc";
+            var endpointSecret = "whsec_stA2aWoTuvWcJv9zII2MSgCTvRmvniUh";
             var signature = HttpContext.Request.Headers["Stripe-Signature"];
             var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-            await _stripe.PaymnentWebHook(json, Request.Headers["Stripe-Signature"], endpointSecret);
+            await _stripe.PaymnentWebHook(json, signature, endpointSecret);
 
-            return Ok();
+            return Ok("Done");
         }
 
     }
