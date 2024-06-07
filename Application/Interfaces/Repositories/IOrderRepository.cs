93010035b233f1e3621a6f8a6cadf35f690a8205
Hammad Ali <127793467+mham7@users.noreply.github.com>
2024-06-07 13:12:47 +0500

    s

---
+++ Application/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Models.SupabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces.Repositories
+{
+    public interface IOrderRepository: IGenericRepository<Order>
+    {
+        Task<object> GetOrderList();
+    }
+}
